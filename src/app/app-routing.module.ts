b23a416f2ceee2625a211ad494885a715b8c8a14
Vaman Patel <vamankumar456@gmail.com>
2020-10-21 11:19:48 +0530

    Some what chanages

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,16 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { ConstructionComponent } from './construction/construction.component';
+import { HomeComponent } from './home/home.component';
 import { LoginformComponent } from './loginform/loginform.component';
 import { SignupformComponent } from './signupform/signupform.component';
 
 const routes: Routes = [
+  { path : '', redirectTo :'/home' , pathMatch:'full'},
+  { path : 'home', component : HomeComponent},
   { path: 'signup', component : SignupformComponent},
-  { path : 'login', component : LoginformComponent}
+  { path : 'login', component : LoginformComponent},
+  { path : 'underwork', component : ConstructionComponent}
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,13 +7,17 @@ import { SignupformComponent } from './signupform/signupform.component';
 import { HeaderComponent } from './header/header.component';
 import { FormsModule } from '@angular/forms';
 import { LoginformComponent } from './loginform/loginform.component';
+import { HomeComponent } from './home/home.component';
+import { ConstructionComponent } from './construction/construction.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     SignupformComponent,
     HeaderComponent,
-    LoginformComponent
+    LoginformComponent,
+    HomeComponent,
+    ConstructionComponent
   ],
   imports: [
     BrowserModule,
